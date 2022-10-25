43b11acda702e3c8958e6231d1cddc0acaf37314
=oxedom <oxedom@gmail.com>
2022-10-25 13:53:11 +0300

    added bootstrap to npm and added button to homepage

--- src/comps/Homepage.js
+++ src/comps/Homepage.js
@@ -1,6 +1,14 @@
+import { Link } from "react-router-dom";
+import { Button } from 'react-bootstrap'
+
 const HomePage = () => {
     return ( <div>
         <h1> Hello From Homepage</h1>
+        <Button className="square-btn">
+            <Link to='shop'>
+            Take me to Shop
+            </Link>
+             </Button>
     </div> );
 }
  
