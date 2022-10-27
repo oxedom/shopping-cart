f1fd73b9ac0a0ef23861ff1d1c795c67bc84bdac
=oxedom <oxedom@gmail.com>
2022-10-27 15:12:19 +0300

    added lazy loading attribute to image + alt for screenreaders

--- src/comps/ProductCard.js
+++ src/comps/ProductCard.js
@@ -2,14 +2,14 @@ import { Link } from "react-router-dom";
 
 
 const ProductCard = (props) => {
-  const { name, src: imgsrc, id, price } = props;
+  const {src: imgsrc, id, name} = props;
 
 
   return (
     <div className="card">
       
       <Link state={{fromShopRoute: true, prodProps: props}} to={{pathname: `/shop/product/${id}`}}>
-      <img className="card-img" src={imgsrc} alt="Card image cap"></img>
+      <img className="card-img" loading="lazy" src={imgsrc} alt={name}></img>
       </Link>
   
     </div>
