c0ea841c80c3ee19b9733fce44aca58b249f5ab2
SamLinux <oxedom@gmail.com>
2022-11-04 12:57:59 +0200

    added basename to browser router

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -10,7 +10,7 @@ const RouteSwitch = (props) => {
   const { cart, setCart } = props.props;
 
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <Nav cart={cart} />
       <Routes>
         <Route exact path="/" element={<HomePage />} />
