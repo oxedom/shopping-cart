c07e94816dc55569b1922c1462e4ff43646999b1
SamLinux <oxedom@gmail.com>
2022-11-04 12:55:02 +0200

    aded browser router with basrname

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -10,7 +10,7 @@ const RouteSwitch = (props) => {
   const { cart, setCart } = props.props;
 
   return (
-    <HashRouter>
+    <BrowserRouter basename="/">
       <Nav cart={cart} />
       <Routes>
         <Route exact path="/" element={<HomePage />} />
@@ -28,7 +28,7 @@ const RouteSwitch = (props) => {
           element={<Checkout cart={cart} setCart={setCart} />}
         />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 
